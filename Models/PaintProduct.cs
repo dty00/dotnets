4791c13fac9732f0935cc1d853a0ce5e0a79fdaf
Tianyi Ding <107295017+dty00@users.noreply.github.com>
2026-06-09 14:01:17 +1000

    Add PaintProduct model

---
+++ Models/PaintProduct.cs
@@ -0,0 +1,25 @@
+using System;
+using dotnets.Enums;
+
+namespace dotnet_test.Models;
+
+
+
+// access modifiers 访问修饰符，控制参数，类，方法访问权限
+
+// public --> 允许外部访问
+//private --> 只允许内部访问
+
+
+public class PaintProduct
+{
+    //参数要大写
+    //name, price,type
+    public string Name;
+    decimal Price;
+    PaintType PaintType;
+
+    //方法
+
+
+}
