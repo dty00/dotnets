232eeca038bcf436fd5e70a6ddfde984f468fa60
Tianyi Ding <107295017+dty00@users.noreply.github.com>
2026-06-09 14:01:16 +1000

    Add Program.cs

---
+++ Program.cs
@@ -0,0 +1,42 @@
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using dotnet_test.Models;
+using dotnets.Enums;
+string[] names = { "Alice", "Bob", "Charlie" };
+
+foreach (var item in names)
+{
+    System.Console.WriteLine(item);
+}
+
+
+
+PaintType paintType = PaintType.HighSheen;
+
+
+switch (paintType)
+{
+    case PaintType.Basic:
+    System.Console.WriteLine("PaintType is basic");
+    break;
+
+    case PaintType.LowSheen:
+    System.Console.WriteLine("PaintType is LowSheen");
+    break;
+
+    case PaintType.HighSheen:
+    System.Console.WriteLine("PaintType is highSheen");
+    break;
+
+    default:
+    System.Console.WriteLine("PaintType is N/A");
+    break;
+}
+
+// 新对象 创建一个实体
+// new PaintProduct() 是一个完整的实体/实例/对象
+
+PaintProduct paintProduct = new PaintProduct();
+
+System.Console.WriteLine(paintProduct.Name);
